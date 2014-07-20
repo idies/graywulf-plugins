1dbab2a2135c74e3715084ccb220fb4da2f21c80
Laszlo Dobos <dobos@complex.elte.hu>
2014-07-20 18:47:56 -0400

    Initial git check-in.

---
+++ src/Jhu.Graywulf.SimpleRestClient/Properties/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Jhu.Graywulf.SimpleRestClient")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("JHU IDIES")]
+[assembly: AssemblyProduct("Jhu.Graywulf.SimpleRestClient")]
+[assembly: AssemblyCopyright("Copyright © JHU IDIES 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8b1b2521-a260-4de5-a5fc-aedd0596c96b")]
+
+[assembly: AssemblyVersion("1.0.*")]
