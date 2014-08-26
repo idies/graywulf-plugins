1c36e06d2267339084227c4a1a075ebdf2d2e3b9
Laszlo Dobos <dobos@complex.elte.hu>
2014-08-25 23:16:27 -0400

    Added casjobs integration plugin skeleton.

---
+++ src/Jhu.Graywulf.Plugins/Schema/CasJobsSettings.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace Jhu.Graywulf.Schema
+{
+    public class CasJobsSettings
+    {
+        #region Private member variables
+
+        #endregion
+        #region Properties
+
+        #endregion
+        #region Constructors and initializers
+
+        public CasJobsSettings()
+        {
+            InitializeMembers(new StreamingContext());
+        }
+
+        [OnDeserializing]
+        private void InitializeMembers(StreamingContext context)
+        {
+        }
+
+        #endregion
+    }
+}
---
+++ src/Jhu.Graywulf.Plugins/Schema/CasJobsUserDatabaseFactory.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Jhu.Graywulf.Registry;
+using Jhu.Graywulf.Schema;
+
+namespace Jhu.Graywulf.Schema
+{
+    public class CasJobsUserDatabaseFactory : UserDatabaseFactory
+    {
+        #region Private member variables
+
+        private CasJobsSettings settings;
+
+        #endregion
+        #region Properties
+
+        public CasJobsSettings Settings
+        {
+            get { return settings; }
+        }
+
+        #endregion
+        #region Constructors and initializers
+
+        public CasJobsUserDatabaseFactory(Federation federation)
+            :base(federation)
+        {
+            InitializeMembers();
+
+            settings = (CasJobsSettings)federation.Settings["CasJobsSettings"].Value;
+        }
+
+        private void InitializeMembers()
+        {
+            this.settings = new CasJobsSettings();
+        }
+
+        #endregion
+
+        public override void EnsureUserDatabaseExists(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override DatasetBase GetUserDatabase(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
