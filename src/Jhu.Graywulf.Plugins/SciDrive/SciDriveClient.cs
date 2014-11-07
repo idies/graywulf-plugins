c62043b8bc4d31eae5c34f1652ffe90afd8af183
Laszlo Dobos <dobos@complex.elte.hu>
2014-11-07 17:07:36 +0100

    SciDriveClient skeleton added.

---
+++ src/Jhu.Graywulf.Plugins/SciDrive/SciDriveClient.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+
+namespace Jhu.Graywulf.SciDrive
+{
+    public class SciDriveClient
+    {
+        public static SciDriveClientConfiguration Configuration
+        {
+            get
+            {
+                return (SciDriveClientConfiguration)ConfigurationManager.GetSection("Jhu.Graywulf/sciDrive");
+            }
+        }
+
+        private static bool IsUriSciDrive(Uri uri)
+        {
+            var c = StringComparer.InvariantCultureIgnoreCase;
+
+            // If URI starts with scidrive:/ or when
+            // http or https is used but host name equals to config
+            if (c.Compare(uri.Scheme, Constants.UriSchemeSciDrive) == 0)
+            {
+                return true;
+            }
+            else if (c.Compare(uri.Scheme, Uri.UriSchemeHttp) == 0 ||
+                     c.Compare(uri.Scheme, Uri.UriSchemeHttps) == 0)
+            {
+                // Compare host name with settings
+                var uriHost = uri.Host;
+                var configHost = SciDriveClient.Configuration.BaseUri.Host;
+
+                // If host names match it is a SciDrive instance
+                if (c.Compare(uriHost, configHost) == 0)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private static Uri GetNormalizedUri(Uri uri)
+        {
+            var c = StringComparer.InvariantCultureIgnoreCase;
+
+            if (c.Compare(uri.Scheme, Constants.UriSchemeSciDrive) == 0)
+            {
+                // If scidrive: is used as scheme, replace with URI from settings
+
+                return Util.UriConverter.Combine(SciDriveClient.Configuration.BaseUri, uri.PathAndQuery);
+            }
+            else
+            {
+                // In any other case just use HTTP or HTTPS
+                return uri;
+            }
+        }
+    }
+}
