fadd5af88ff6eba3e0b404128d395f0c2dd4016f
Laszlo Dobos <dobos@complex.elte.hu>
2014-12-24 01:49:17 -0500

    Missing file added.

---
+++ src/Jhu.Graywulf.Plugins/CasJobs/Responses/ErrorResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Jhu.Graywulf.CasJobs
+{
+    [JsonObject(MemberSerialization.OptIn)]
+    internal class ErrorResponse
+    {
+        [JsonProperty("Error Code")]
+        public int StatusCode { get; set; }
+
+        [JsonProperty("Error Type")]
+        public string ErrorType { get; set; }
+
+        [JsonProperty("Error Message")]
+        public string Message { get; set; }
+
+        [JsonProperty("LogMessageID")]
+        public string LogMessageID { get; set; }
+    }
+}
