c4138ef0057ba6eb755f81dc46a8b7b28d8873f7
Laszlo Dobos <dobos@complex.elte.hu>
2015-03-09 17:44:36 -0400

    SciDrive import and export plug-in skeleton added.

---
+++ src/Jhu.Graywulf.Plugins/SciDrive/SciDriveExportTablesJobFactory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Jhu.Graywulf.SciDrive
+{
+    public class SciDriveExportTablesJobFactory : Jhu.Graywulf.Jobs.ExportTables.ExportTablesJobFactory
+    {
+    }
+}
---
+++ src/Jhu.Graywulf.Plugins/SciDrive/SciDriveImportTablesJobFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Jhu.Graywulf.Jobs.ImportTables;
+
+namespace Jhu.Graywulf.SciDrive
+{
+    public class SciDriveImportTablesJobFactory : ImportTablesJobFactory
+    {
+        public override IEnumerable<Jobs.ImportTables.ImportTablesMethod> EnumerateMethods()
+        {
+            foreach (var method in base.EnumerateMethods())
+            {
+                yield return method;
+            }
+
+            yield return new ImportTablesMethod()
+            {
+                ID = "scidrive",
+                Description = "Import from SciDrive",
+                BaseUri = SciDriveClient.Configuration.BaseUri,
+                HasCredentials = false,
+            };
+        }
+
+        public override ImportTablesParameters CreateParameters(Registry.Federation federation, Uri uri, IO.Credentials credentials, Format.DataFileBase source, IO.Tasks.DestinationTable destination)
+        {
+            // TODO: intercept scidrive URIs and modify credentials
+
+            return base.CreateParameters(federation, uri, credentials, source, destination);
+        }
+    }
+}
