672cb587d3a567d1b7f0faee3536e7307de3d51d
Laszlo Dobos <dobos@complex.elte.hu>
2016-11-30 17:43:11 -0500

    Copy output to web sites automatically for easy debugging.

--- src/Jhu.Graywulf.Plugins/post-build.ps1
+++ src/Jhu.Graywulf.Plugins/post-build.ps1
@@ -17,3 +17,9 @@ cp ${ProjectDir}${OutDir}*.pdb ${SolutionDir}graywulf\test\Jhu.Graywulf.Jobs.Tes
 
 cp ${ProjectDir}${OutDir}*.dll ${SolutionDir}graywulf\test\Jhu.Graywulf.Scheduler.Test\${OutDir}
 cp ${ProjectDir}${OutDir}*.pdb ${SolutionDir}graywulf\test\Jhu.Graywulf.Scheduler.Test\${OutDir}
+
+cp ${ProjectDir}${OutDir}*.dll ${SolutionDir}graywulf\web\Jhu.Graywulf.Web.Auth\bin
+cp ${ProjectDir}${OutDir}*.pdb ${SolutionDir}graywulf\web\Jhu.Graywulf.Web.Auth\bin
+
+cp ${ProjectDir}${OutDir}*.dll ${SolutionDir}graywulf\web\Jhu.Graywulf.Web.UI\bin
+cp ${ProjectDir}${OutDir}*.pdb ${SolutionDir}graywulf\web\Jhu.Graywulf.Web.UI\bin
