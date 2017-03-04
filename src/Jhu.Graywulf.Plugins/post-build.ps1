c0bf912f1932dfca2aca417df8e4fb916d582c7a
Laszlo Dobos <dobos@complex.elte.hu>
2017-03-03 19:18:18 -0500

    New build system.

--- src/Jhu.Graywulf.Plugins/post-build.ps1
+++ src/Jhu.Graywulf.Plugins/post-build.ps1
@@ -1,13 +1,4 @@
-﻿param(
-	[string]$SolutionDir,
-	[string]$SolutionName,
-	[string]$ProjectDir,
-	[string]$ProjectName,
-	[string]$OutDir,
-	[string]$TargetName
-)
-
-cp $ProjectDir$OutDir$TargetName.dll $SolutionDir$OutDir
+﻿cp $ProjectDir$OutDir$TargetName.dll $SolutionDir$OutDir
 cp $ProjectDir$OutDir$TargetName.pdb $SolutionDir$OutDir
 
 # copy to test
